0e547d80cc86c3edf81b0432a4dd4124d0f76943
Nishita Sahajwani <nishitasahajwani1401@gmail.com>
2023-02-06 06:41:15 -0800

    initialCOmmit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  var rng = Random();
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _incrementByfive() {
+    setState(() {
+      _counter = _counter + 5;
+    });
+  }
+
+  void _decrementByfive() {
+    setState(() {
+      _counter = _counter - 5;
+    });
+  }
+
+  void incrementByRandom() {
+    setState(() {
+      _counter = _counter + rng.nextInt(15);
+    });
+  }
+
+  void decrementByRandom() {
+    setState(() {
+      _counter = _counter - rng.nextInt(15);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+                onPressed: _incrementByfive, child: Text("Increment 5")),
+            ElevatedButton(
+                onPressed: _decrementByfive, child: Text("Decrement 5")),
+            ElevatedButton(
+                onPressed: incrementByRandom, child: Text("Increment random")),
+            ElevatedButton(
+                onPressed: decrementByRandom, child: Text("Decrement random")),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
